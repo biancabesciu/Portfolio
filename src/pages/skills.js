4ace25f664d78b6ad143118ec9dc1747173421ce
Bianca Besciu <bianca.besciu@gmail.com>
2018-02-13 18:30:29 +0100

    working on skills page

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -3,15 +3,23 @@ import React, { Component } from 'react';
 class Skills extends Component {
     render() {
         return (
-            <section id="services">
+            <section id="skills">
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-12 text-center">
                             <h2 className="section-heading">SKILLS</h2>
-                            <span className="skills">MY KNOWLEDGE LEVEL IN&nbsp;SOFTWARE</span>
+                            <span className="skills-span">MY KNOWLEDGE LEVEL IN&nbsp;SOFTWARE</span>
                             <hr className="my-4"></hr>
                         </div>
                     </div>
+                </div>
+
+                <div className="skills-bar">
+
+                </div>
+
+                <div className="skills-text">
+
                 </div>
             </section>
 
